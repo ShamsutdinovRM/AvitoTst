fca3a421b84b332d9a296b9ef17405ed4ae9fa40
protegos <protego081200@gmail.com>
2022-09-28 03:23:53 +0500

    Add insert fields

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,4 +1,9 @@
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     balance DECIMAL CHECK ( balance >= 0 )
-)
+);
+
+INSERT INTO users (id) VALUES ('first');
+INSERT INTO users (id) VALUES ('second');
+INSERT INTO users (id) VALUES ('third');
+INSERT INTO users (id) VALUES ('forty');
